aec981029ca8d67de1214f48be928199bdaefc85
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-08-26 10:56:20 -0400

    Added some comments. Updated to show how to use ApplicationContext container.

--- WithXML/xml/injection/constructor/Message.java
+++ WithXML/xml/injection/constructor/Message.java
@@ -1,8 +1,12 @@
 package xml.injection.constructor;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+// Requires the following jars: spring-beans.jar, spring-context.jar, spring-core.jar, spring-expression.jar
+// Also needs Apache Commons Logging: commongs-logging.jar
 public class Message {
   private String msg = null;
 
@@ -15,8 +19,12 @@ public class Message {
   }
 
   public static void main(String[] args) {
-    XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("/xml/injection/resource/spring.xml"));
-    Message msg = (Message) factory.getBean("constructor-injection");
+    // XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("/xml/injection/resource/spring.xml"));
+    // Message msg = (Message) factory.getBean("constructor-injection");
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("/xml/injection/resource/spring.xml");
+    Message msg = (Message) context.getBean("constructor-injection");
+
     System.out.println(msg.getMessage());
   }
 }
